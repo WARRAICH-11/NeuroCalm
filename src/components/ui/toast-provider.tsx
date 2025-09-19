4d01bed34d428cd7f1516fc87c25149747b1ac39
HASSAN WARRAICH <hm2257153@gmail.com>
2025-09-19 11:14:45 +0500

    toast

--- src/components/ui/toast-provider.tsx
+++ src/components/ui/toast-provider.tsx
@@ -1,12 +1,13 @@
 'use client';
 
 import { Toaster } from '@/components/ui/toaster';
+import { ToastProvider as RadixToastProvider } from '@/components/ui/toast-context';
 
 export function ToastProvider({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <RadixToastProvider>
       {children}
       <Toaster />
-    </>
+    </RadixToastProvider>
   );
 }
